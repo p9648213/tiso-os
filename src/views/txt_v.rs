64cc9e475d03773e01b6e87d15f50402bc936875
p9648213 <p9648213750912@gmail.com>
2025-07-03 07:15:30 +0700

    render txt - pending

--- src/views/txt_v.rs
+++ src/views/txt_v.rs
@@ -52,8 +52,8 @@ pub fn render_txt_window(
     parent_height: i32,
     parent_width: i32,
 ) -> impl Renderable {
-    let window_width = 300;
-    let window_height = 200;
+    let window_width = 600;
+    let window_height = 500;
 
     let left = ((parent_width / 2) - (window_width / 2)).max(0);
     let top = ((parent_height / 2) - (window_height / 2)).max(0);
@@ -69,13 +69,18 @@ pub fn render_txt_window(
         )))
         div
             id={ "txt-window-" (txt_id) }
-            class="absolute bg-white shadow-lg"
+            class="absolute flex flex-col bg-zinc-800 rounded-sm overflow-hidden text-white"
             style={ "top:" (top) "px; left:" (left) "px; width:" (window_width) "px; height:" (window_height) "px;" }
         {
-            div id={ "txt-header-" (txt_id) } class="flex items-center bg-red-300 px-2 h-5 select-none" {
+            div id={ "txt-header-" (txt_id) } class="flex items-center bg-zinc-900 px-3 h-10 select-none" {
+                img class="mr-2 w-5 h-5" src="/assets/images/text-editor.svg" draggable="false";
                 (file_name)
             }
-            div class="p-2" { "Content" }
+            div class="px-3 py-2 h-full" {
+                textarea id={"txt-editor-" (txt_id)} class="focus-visible:border-none focus-visible:outline-none w-full h-full resize-none" {
+                    "Hello, world!"
+                }
+            }
         }
     }
 }
