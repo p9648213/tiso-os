b3a515eef9f61094b462094e411aeff7a90e14e6
p9648213 <p9648213750912@gmail.com>
2026-01-04 16:03:43 +0700

    rm

--- src/utilities/terminal_rm.rs
+++ src/utilities/terminal_rm.rs
@@ -1,5 +1,11 @@
 use deadpool_postgres::Pool;
 
+use crate::models::{
+    file_db::File,
+    folder_db::{Folder, FolderSortType},
+    folder_item::{FolderItem, ItemType},
+};
+
 pub struct Rm<'a> {
     pub current_dir: String,
     pub args: &'a Vec<String>,
@@ -22,8 +28,47 @@ impl<'a> Rm<'a> {
             self.args.join(" ")
         } else {
             self.args[0].trim().to_string()
-        }; 
+        };
 
-        Ok(format!("{} removed", item_name))
+        if self.current_dir == "/" {
+            Err(r#"Cannot remove folder in path "/". Use "cd" to change directory."#.to_string())
+        } else {
+            let current_folder =
+                Folder::get_folder_by_path(&self.current_dir, self.user_id, vec!["id"], self.pool)
+                    .await
+                    .map_err(|err| err.to_string())?;
+
+            let folder_items = FolderItem::get_folder_items(
+                current_folder.id.unwrap(),
+                self.user_id,
+                &FolderSortType::Custom,
+                self.pool,
+            )
+            .await
+            .map_err(|err| err.to_string())?;
+
+            let deleted_item = folder_items
+                .iter()
+                .find(|item| item.name.as_ref().unwrap() == &item_name);
+
+            if let Some(deleted_item) = deleted_item {
+                match deleted_item.item_type.as_ref().unwrap() {
+                    ItemType::File => {
+                        File::delete_file(deleted_item.id.unwrap(), self.user_id, self.pool)
+                            .await
+                            .map_err(|err| err.to_string())?;
+                    }
+                    ItemType::Folder => {
+                        Folder::delete_folder(deleted_item.id.unwrap(), self.user_id, self.pool)
+                            .await
+                            .map_err(|err| err.to_string())?;
+                    }
+                }
+            } else {
+                return Err(format!("Not found: {}", item_name));
+            }
+
+            Ok(format!("{} removed", item_name))
+        }
     }
 }
